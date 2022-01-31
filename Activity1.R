d2af34279a76dc3b77990bd823fbb4c41c97018a
atheophilus <atheophilus@colgate.edu>
2022-01-31 11:55:41 -0500

    activity 2 examples

--- Activity1.R
+++ Activity1.R
@@ -1,2 +1,8 @@
 print("Hello World")
 # my new script
+# remember that this is a comment that R will ignore
+# it's extremely important to use comments for documenting your code and writing notes
+# let's calculate 6 raised to the 6 power
+6^6
+#234 + 8
+234+8
---
+++ activity 2 practice.R
@@ -0,0 +1,21 @@
+#GEOG331 activity 2 practice script
+# AT 01/31/22
+
+
+# make a vector of tree heights
+heights <- c(30,41,20,22)
+# vector notation = brackets
+
+# converting heights to cm
+heights_cm <- heights*100
+
+# matrices are what we will work with the most in R
+# use "help" or question mark "?" with function name in terminal for instructions
+
+Mat<-matrix(c(1,2,3,4,5,6),ncol=2, byrow=TRUE)
+
+
+
+datw <- read.csv("Z:\\GEOG331_S22\data\noaa_weather\2011124.csv")
+
+
