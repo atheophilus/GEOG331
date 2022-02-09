4b30f2707b83c967a7a5c657fffe53a1f35c4dc3
atheophilus <atheophilus@colgate.edu>
2022-02-09 12:26:11 -0500

    Activity 2 2/9 Updates

--- Activity 2 Real.R
+++ Activity 2 Real.R
@@ -1,11 +1,15 @@
 setwd("T:/students/atheophilus/Data/noaa_weather")
 getwd()
-datW <- read.csv("T:\\students\\atheophilus\\Data\\noaa_weather\\2011124.csv")
+datW <- read.csv("T:\\students\\atheophilus\\Data\\noaa_weather\\2011124.csv", stringsAsFactors = T)
+
 #get more information about the dataframe
 str(datW)
+
 #specify a column with a proper date format
 #note the format here dataframe$column
 datW$dateF <- as.Date(datW$DATE, "%Y-%m-%d")
+#GOOGLE DATE FORMATTING IN R TO FIND MORE OPTIONS
+
 #google date formatting in r to find more options and learn more
 #create a date column by reformatting the date to only include years
 #and indicating that it should be treated as numeric data
@@ -24,8 +28,10 @@ as.numeric(c("-15"," 29 ","35"))
 
 #find out all unique site names
 unique(datW$NAME)
+
 #look at the mean maximum temperature for Aberdeen
 mean(datW$TMAX[datW$NAME == "ABERDEEN, WA US"])
+
 #look at the mean maximum temperature for Aberdeen
 #with na.rm argument set to true to ingnore NA
 mean(datW$TMAX[datW$NAME == "ABERDEEN, WA US"], na.rm=TRUE)
@@ -33,6 +39,7 @@ mean(datW$TMAX[datW$NAME == "ABERDEEN, WA US"], na.rm=TRUE)
 #calculate the average daily temperature
 #This temperature will be halfway between the minimum and maximum temperature
 datW$TAVE <- datW$TMIN + ((datW$TMAX-datW$TMIN)/2)
+
 #get the mean across all sites
 #the by function is a list of one or more variables to index over.
 #FUN indicates the function we want to use
@@ -40,21 +47,26 @@ datW$TAVE <- datW$TMIN + ((datW$TMAX-datW$TMIN)/2)
 #here we want to use the na.rm arguments specific to 
 averageTemp <- aggregate(datW$TAVE, by=list(datW$NAME), FUN="mean",na.rm=TRUE)
 averageTemp
+
 #change the automatic output of column names to be more meaningful
 #note that MAAT is a common abbreviation for Mean Annual Air Temperature
 colnames(averageTemp) <- c("NAME","MAAT")
 averageTemp
+
 #convert level to number for factor data type
 #you will have to reference the level output or look at the row of data to see the character designation.
+#datW$siteN <- as.factor(datW$NAME)
 datW$siteN <- as.numeric(datW$NAME)
-suppressWarnings(as.numeric(datW$NAME))
+
+
+
 #make a histogram for the first site in our levels, Aberdeen
 #main= is the title name argument.
 #Here you want to paste the actual name of the factor not the numeric index
 #since that will be more meaningful. 
 hist(datW$TAVE[datW$siteN == 1],
      freq=FALSE, 
-     main ="ABERDEEN, WA US",
+     main = "ABERDEEN, WA US",
      xlab ="Average daily temperature (degrees C)", 
      ylab="Relative frequency",
      col="grey50",
