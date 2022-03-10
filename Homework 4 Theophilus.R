74a04a765ee60df48b34ce8d86a0446a6751cb89
atheophilus <atheophilus@colgate.edu>
2022-03-10 18:00:30 -0500

    Final code activity 4

--- Homework 4 Theophilus.R
+++ Homework 4 Theophilus.R
@@ -1,5 +1,5 @@
-#use built in iris dataset
-#take a look at it
+# Alex Theophilus GEOG331 
+# Activity 4
 head(iris)
 #load in some tidyverse packages
 library(dplyr)
@@ -17,29 +17,29 @@ library(ggplot2)
 #2. iris  petal length x width
 #3. iris sepal length x petal length
 
-# hint: consider using a list, and also new vectors for regression variables
-iris
-# build regressions
 flower <- iris[iris$Species=="versicolor",]
-fit1 <- lm(flower$Sepal.Length ~ flower$Sepal.Width)
-fit2 <- lm(flower$Petal.Length ~ flower$Petal.Width)
-fit3 <- lm(flower$Sepal.Length ~ flower$Petal.Length)
-# get regressions in for loop
 
+Q1_Regressions = list()
 
+ordered_attributes = list(flower$Sepal.Length, flower$Sepal.Width,
+                          flower$Petal.Length, flower$Petal.Width,
+                          flower$Sepal.Length, flower$Petal.Length)
+
+for (i in 1:3){
+  List1 = unlist(ordered_attributes[((i*2)-1)])
+  List2 = unlist(ordered_attributes[(i*2)])
+  Q1_Regressions[[i]] <- lm(List1 ~ List2)
+}
 
 #####################################
 ##### Part 2: data in dplyr     #####
 #####################################
 
-#use dplyr to join data of maximum height
-#to a new iris data frame
 height <- data.frame(Species = c("virginica","setosa","versicolor"),
                      Height.cm = c(60,100,11.8))
 
-full_join(height$Species, height$Height.cm, by=NULL, copy=FALSE,
-          suffix = c(".x", ".y"), keep = FALSE, na_matches="na")
-
+iris %>%
+  inner_join(y=height, by=c("Species"))
 
 #####################################
 ##### Part 3: plots in ggplot2  #####
@@ -49,16 +49,21 @@ full_join(height$Species, height$Height.cm, by=NULL, copy=FALSE,
 plot(iris$Sepal.Length,iris$Sepal.Width)
 
 #3a. now make the same plot in ggplot
-
+ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width)) + 
+  geom_point(size=2, shape=1)
 
 #3b. make a scatter plot with ggplot and get rid of  busy grid lines
-
+ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width)) + 
+  geom_point(size=2, shape=1) +
+  theme_classic()
 
 #3c. make a scatter plot with ggplot, remove grid lines, add a title and axis labels, 
 #    show species by color, and make the point size proportional to petal length
+ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width, size=Petal.Length, color=Species)) + 
+  geom_point() +
+  theme_classic() +
+  labs(title="Plot of Iris Dimensions",
+       x="Sepal Length", y="Sepal Width")
 
-#####################################
-##### Question: how did         #####
-##### arguments differ between  #####
-##### plot and ggplot?          #####
-#####################################		
+# final question answered in google doc
+# end of code	
