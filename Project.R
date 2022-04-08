fca7025e8222057ffd449ac7e97cfad6d3967ca6
atheophilus <atheophilus@colgate.edu>
2022-04-08 12:03:03 -0400

    PROJECT CSV FILES

---
+++ Project.R
@@ -0,0 +1,7 @@
+# read in project csv
+# edgar food emissions
+
+FoodGHG <- read.csv("T:\\students\\atheophilus\\Data\\Edgar_Food_Data\\EdgarFoodGHGShares.csv", skip = 3)
+AmmoniaPollution <- read.csv("T:\\students\\atheophilus\\Data\\Edgar_Food_Data\\Edgar-Ammon.csv")
+
+
